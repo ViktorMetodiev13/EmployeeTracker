4a34965f102087f345c33aa31053d7ae23faf7e1
ViktorMetodiev13 <miroslavov_victor_@abv.bg>
2024-08-13 23:20:37 +0300

    Changes

--- EmployeeTracker/client/src/components/Header/Header.tsx
+++ EmployeeTracker/client/src/components/Header/Header.tsx
@@ -7,7 +7,7 @@ export const Header: React.FC = () => {
     return (
         <nav className="navbar navbar-expand-lg bg-body-tertiary">
             <div className="container-fluid">
-                <Link className="navbar-brand" to="/"><img src='.././images/logo.png' alt="Employee Tracker" className="nav-logo-img"/></Link>
+                <Link className="navbar-brand" to="/"><img src='.././images/logo.png' alt="Employee Tracker" className="nav-logo-img" /></Link>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
@@ -16,8 +16,10 @@ export const Header: React.FC = () => {
                         <li className="nav-item">
                             <Link className="nav-link active" aria-current="page" to="/employees">Employees</Link>
                         </li>
+                        <li className="nav-item">
+                            <Link className="nav-link active" aria-current="page" to="/addEmployee">Add Employee</Link>
+                        </li>
                     </ul>
-                    <button className="btn btn-outline-success" type="submit">AddEmployee</button>
                 </div>
             </div>
         </nav>
--- EmployeeTracker/client/src/components/ListOfEmployees/ListOfEmployees.tsx
+++ EmployeeTracker/client/src/components/ListOfEmployees/ListOfEmployees.tsx
@@ -2,6 +2,43 @@ import React from "react"
 
 export const ListOfEmployees: React.FC = () => {
     return (
-        <div>List of Employees</div>
+        <table className="table table-striped">
+            <thead>
+                <tr>
+                    <th scope="col">ID</th>
+                    <th scope="col">Employee</th>
+                    <th scope="col">Email</th>
+                    <th scope="col">Role</th>
+                    <th scope="col">Started Working In</th>
+                    <th scope="col">Actions</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>1</td>
+                    <td>Viktor Metodiev</td>
+                    <td>metodievviktor1@gmail.com</td>
+                    <td>Team Lead</td>
+                    <td>December 2021</td>
+                    <td>delete, edit</td>
+                </tr>
+                <tr>
+                    <td>2</td>
+                    <td>Stefan Yordanov</td>
+                    <td>stYordanov@gmail.com</td>
+                    <td>Senior Web Dev</td>
+                    <td>March 2022</td>
+                    <td>delete, edit</td>
+                </tr>
+                <tr>
+                    <td>3</td>
+                    <td>Yordan Vladimirov</td>
+                    <td>yoVladimirov@gmail.com</td>
+                    <td>CEO</td>
+                    <td>November 2018</td>
+                    <td>delete, edit</td>
+                </tr>
+            </tbody>
+        </table>
     )
 }
